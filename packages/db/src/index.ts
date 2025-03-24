9d57c3c42b7cff1339aea69ebb25d6358cf75159
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-03-24 09:57:34 +0100

    :tada:

---
+++ packages/db/src/index.ts
@@ -0,0 +1,72 @@
+// export * from "drizzle-orm/sql";
+// export { alias } from "drizzle-orm/pg-core";
+/* eslint-disable no-restricted-properties */
+
+// Solution for prisma edge: @link https://github.com/prisma/prisma/issues/22050#issuecomment-1821208388
+// import { PrismaClient } from "@prisma/client/edge";
+// import { withAccelerate } from "@prisma/extension-accelerate";
+
+import {
+  Prisma as BasePrisma,
+  PrismaClient,
+  Roles,
+  Users,
+} from "@prisma/client";
+
+export * from "@prisma/client";
+// Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? [
+            // "query",
+            "error",
+            "warn",
+          ]
+        : ["error"],
+  }).$extends({
+    query: {
+      $allModels: {
+        // async $allOperations({args,operation})
+        // {
+        // },
+        async findFirst({ model, operation, args, query }) {
+          if (!args) args = { where: {} };
+          if (!args.where) args.where = {};
+
+          if (!Object.keys(args.where).includes("deletedAt"))
+            args.where = { deletedAt: null, ...args.where };
+          // args.where = {};
+          // console.log(args.where);
+          return query(args);
+        },
+        async findMany({ model, operation, args, query }) {
+          if (!args) args = { where: {} };
+          if (!args.where) args.where = {};
+
+          if (!Object.keys(args.where).includes("deletedAt"))
+            args.where = { deletedAt: null, ...args.where };
+          // args.where.deletedAt = null;
+
+          // args.where = {};
+          // console.log(args.where);
+          return query(args);
+        },
+      },
+    },
+  });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export const Prisma = BasePrisma;
+export { PrismaClient };
+export type { Roles, Users };
