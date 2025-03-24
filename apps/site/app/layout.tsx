2f7af23d43762649d7edd0b2a94188c1c9a74b4c
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-03-24 14:18:05 +0100

    :tada:

--- apps/site/app/layout.tsx
+++ apps/site/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        HELLO WO
       </body>
     </html>
   );
