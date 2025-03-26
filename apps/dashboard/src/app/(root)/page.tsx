b089e408dd05b75bdcfa2bae6132eac9a36b591b
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-03-26 12:53:01 +0100

    :tada:

---
+++ apps/dashboard/src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "@gnd/ui/button";
+export default function Page({}) {
+  return (
+    <div>
+      <Button>Hello</Button>
+      Hello World
+    </div>
+  );
+}
